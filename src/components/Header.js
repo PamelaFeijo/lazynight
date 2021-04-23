a5ea09c5860c5c5c7615fa590e91103a5f620cc5
PamelaFeijo <pamelaffeijo@gmail.com>
2021-04-23 12:32:49 +0200

    Add Title

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,30 +4,17 @@ import Title from "./Title";
 import QuizButton from "./QuizButton";
 import { useLocation } from "react-router-dom";
 
-
 const Header = () => {
-   
-    const location = useLocation();
-   
-   /*  useEffect(() => {
-        console.log(location)
-        if (location.pathname === "/"){
-            console.log("Home")}
-    }, []) */
-    
+  const location = useLocation();
 
   return (
     <div>
-      {/* <h1>{location.pathname.replace("/", "Hey there! Don't know what to do")}</h1> */}
-     {/*  <span>Path is: {location.pathname}</span> */}
       <Navbar />
-      <Banner location={location.pathname}/>
-      <Title />
+      <Banner location={location.pathname} />
+      <Title location={location.pathname} />
       <QuizButton />
     </div>
   );
 };
 
 export default Header;
-
-
