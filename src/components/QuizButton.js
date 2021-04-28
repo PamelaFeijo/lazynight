4c16237629f30fce078e1b03b9149656a410ef30
Francesco <france.luciani@gmail.com>
2021-04-28 15:19:48 +0200

    merged

--- src/components/QuizButton.js
+++ src/components/QuizButton.js
@@ -2,9 +2,18 @@ import "./QuizButton.css";
 
 const QuizButton = () => {
   return (
-    <div> 
-      <button className="quiz-button">I DON'T KNOW <br></br>WHAT TO EAT</button>
-    </div>   
+    <div>
+      <div>
+        <button className="quiz-button">
+          I DON'T KNOW <br></br>WHAT TO EAT
+        </button>
+      </div>
+      <div>
+        <button className="quiz-button">
+          I DON'T KNOW <br></br>WHAT TO WATCH
+        </button>
+      </div>
+    </div>
   );
 };
 export default QuizButton;
